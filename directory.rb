fbb27cb53a4806b034e149d2c6094929af9d7f89
Fabri <yannis.fabri@cgi.com>
2018-09-17 16:54:52 +0100

    Iteration Loop and Array

--- directory.rb
+++ directory.rb
@@ -1,14 +1,23 @@
+
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
+#Print the list of students
 puts "The students of Villains Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-	
+students.each do |student|
+  puts student
+end
+#Print the total number of students
+print "Overall, we have #{students.count} great students "
+#print doesn't add new line charcters
